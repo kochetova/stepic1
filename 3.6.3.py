a64a74c75026cac3037c2b87cd84b796b55b8ec4
kochetova <daria.d.igumnova@gmail.com>
2020-04-01 21:24:05 +0300

    Renamed file for 3.6.2

---
+++ 3.6.3.py
@@ -0,0 +1,11 @@
+import requests
+
+with open('/Users/igumnyash/Downloads/dataset_3378_3.txt') as link:
+    url = link.readline().strip()
+print(url)
+
+while requests.get(url).text.split()[0] != 'We':
+    url = 'https://stepic.org/media/attachments/course67/3.6.3/' + requests.get(url).text
+    print(url)
+
+print(requests.get(url).text)

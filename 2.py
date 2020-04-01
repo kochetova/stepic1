9d020470ae7a85afc1e57dc862a0460ef9e7eeec
kochetova <daria.d.igumnova@gmail.com>
2020-04-01 16:47:34 +0300

    Updated 3.6.2 and changed user

--- 2.py
+++ 2.py
@@ -1,8 +1,8 @@
+import requests
+
 with open('/Users/igumnyash/Downloads/dataset_3378_2.txt') as link:
     url = link.readline().strip()
 print(url)
-
-import requests
 
 file = requests.get(url)
 print(len(file.text.splitlines()))
